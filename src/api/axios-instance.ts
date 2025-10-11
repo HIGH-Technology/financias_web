9ba9224fd7929f5d28786b9ea6cdf38b8d2b0fce
vbasilioo <vbasilio2019@gmail.com>
2025-10-10 22:18:13 -0300

    fix: remove orval config in git & add in gitignore; remove mocked axios instance

--- src/api/axios-instance.ts
+++ src/api/axios-instance.ts
@@ -1,7 +1,7 @@
 import Axios, { AxiosError, AxiosRequestConfig } from 'axios';
 
 export const AXIOS_INSTANCE = Axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api',
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
 AXIOS_INSTANCE.interceptors.request.use((config) => {
